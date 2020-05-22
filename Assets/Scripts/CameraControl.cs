bc9bb57b45dc00bf7861b173f14d927ca6d245e5
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-05-23 00:46:50 +0800

    [Update] Joystick 角色控制

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
@@ -13,12 +14,55 @@ public class CameraControl : MonoBehaviour
     public float xAngleSensitivity = 10;
     public float yAngleSensitivity = 2;
     public float scaleSensitivity = 10;
+    
+    private FloatingJoystick _rotateJoystick;
+    private FloatingJoystick _moveJoystick;
+    private GameObject _joystickObjectRotate;
+    private GameObject _joystickObjectMove;
+
+    private void Awake()
+    {
+        if (lookAtPlayer != null)
+            ControlViewport();
+        if (_rotateJoystick == null)
+        {
+            FloatingJoystick[] joysticks = FindObjectsOfType<FloatingJoystick>();
+            foreach (FloatingJoystick joystick in joysticks)
+            {
+                if (joystick.gameObject.name == "Rotate Joystick")
+                {
+                    _rotateJoystick = joystick;
+                    _joystickObjectRotate = _rotateJoystick.transform.GetChild(0).gameObject;
+                }else if (joystick.gameObject.name == "Move Joystick")
+                {
+                    _moveJoystick = joystick;
+                    _joystickObjectMove = _moveJoystick.transform.GetChild(0).gameObject;
+                }
+            }
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (lookAtPlayer != null)
             ControlViewport();
+        if (_rotateJoystick == null)
+        {
+            FloatingJoystick[] joysticks = FindObjectsOfType<FloatingJoystick>();
+            foreach (FloatingJoystick joystick in joysticks)
+            {
+                if (joystick.gameObject.name == "Rotate Joystick")
+                {
+                    _rotateJoystick = joystick;
+                    _joystickObjectRotate = _rotateJoystick.transform.GetChild(0).gameObject;
+                }else if (joystick.gameObject.name == "Move Joystick")
+                {
+                    _moveJoystick = joystick;
+                    _joystickObjectMove = _moveJoystick.transform.GetChild(0).gameObject;
+                }
+            }
+        }
     }
 
     void ControlViewport()
@@ -37,6 +81,32 @@ public class CameraControl : MonoBehaviour
             {
                 Cursor.visible = true;
             }
+            
+            // Android Control
+            if (_rotateJoystick != null)
+            {
+                // rotate viewport
+                if (_rotateJoystick.Horizontal >= 0.5f || _rotateJoystick.Horizontal <= -0.5f)
+                    xAngle -= (_rotateJoystick.Horizontal * xAngleSensitivity / 2);
+                if (_rotateJoystick.Vertical >= 0.5f  || _rotateJoystick.Vertical <= -0.5f)
+                    yAngle -= _rotateJoystick.Vertical;
+                // scale
+                if (Input.touchCount == 2 && 
+                    !_joystickObjectRotate.activeSelf &&
+                    !_joystickObjectMove.activeSelf)
+                {
+                    Vector2 finger1 = Input.touches[0].position;
+                    Vector2 moveFinger1 = finger1 + Input.touches[0].deltaPosition;
+                    Vector2 finger2 = Input.touches[1].position;
+                    Vector2 moveFinger2 = finger2 + Input.touches[1].deltaPosition;
+
+                    float fingerDistance = (finger1 - finger2).magnitude;
+                    float moveFingerDistance = (moveFinger1 - moveFinger2).magnitude;
+                    yDistance -= ((moveFingerDistance - fingerDistance) / (scaleSensitivity * 2));
+
+                }
+            }
+
         }else if (Application.platform == RuntimePlatform.Android)
         {
             // test
--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
@@ -19,6 +16,7 @@ public class CharacterControl : MonoBehaviour
     private float _animaParamSpeed = 0.0f;
     private Vector3 _gravityVelocity = Vector3.zero;
     private bool isGround = false;
+    private FloatingJoystick _moveJoystick;
 
     private void Awake()
     {
@@ -26,33 +24,54 @@ public class CharacterControl : MonoBehaviour
             FindCameraTransform();
         _animator = gameObject.GetComponent<Animator>();
         _characterController = gameObject.GetComponent<CharacterController>();
+        
+        // find joystick
+        if (_moveJoystick == null)
+        {
+            FloatingJoystick[] joysticks = FindObjectsOfType<FloatingJoystick>();
+            foreach (FloatingJoystick joystick in joysticks)
+                if (joystick.gameObject.name == "Move Joystick")
+                    _moveJoystick = joystick;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // find joystick
+        if (_moveJoystick == null)
+        {
+            FloatingJoystick[] joysticks = FindObjectsOfType<FloatingJoystick>();
+            foreach (FloatingJoystick joystick in joysticks)
+                if (joystick.gameObject.name == "Move Joystick")
+                    _moveJoystick = joystick;
+        }
+        
         if (cameraTransform == null)
             FindCameraTransform();
         else
             PlayerMovement();
     }
-
+    
+    
     private void PlayerMovement()
     {
+        // gravity control
+        _gravityVelocity.y += gravity * Time.deltaTime;
+        _characterController.Move(_gravityVelocity * Time.deltaTime);
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGround && _gravityVelocity.y < 0)
+            _gravityVelocity.y = -2f;
+
+        // facing camera forward direction
+        Vector3 facing = (transform.position - cameraTransform.position).normalized;
+        facing.y = 0;
+        Vector3 move = Vector3.zero;
 
         if (Application.platform == RuntimePlatform.WindowsPlayer ||
             Application.platform == RuntimePlatform.WindowsEditor)
         {
-            _gravityVelocity.y += gravity * Time.deltaTime;
-            _characterController.Move(_gravityVelocity * Time.deltaTime);
-            isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-            if (isGround && _gravityVelocity.y < 0)
-                _gravityVelocity.y = -2f;
-
-            Vector3 facing = (transform.position - cameraTransform.position).normalized;
-            facing.y = 0;
-            Vector3 move = Vector3.zero;
-            
+            // Windows Control
             if (Input.GetKey(KeyCode.W))
                 move += facing;
             if (Input.GetKey(KeyCode.A))
@@ -61,30 +80,43 @@ public class CharacterControl : MonoBehaviour
                 move += (-facing);
             if (Input.GetKey(KeyCode.D))
                 move += new Vector3(facing.z, 0, -facing.x);
-            
-            if (move != Vector3.zero)
-            {
-                Quaternion facingRotation = Quaternion.LookRotation(move);
-                transform.rotation = Quaternion.Slerp(
-                    transform.rotation, 
-                    facingRotation, 
-                    facingSpeed * Time.deltaTime);
-                _animaParamSpeed += Time.deltaTime * speed;
-                _characterController.Move(move * speed * Time.deltaTime);
-            }
-            else
-            {
-                _animaParamSpeed -= Time.deltaTime * speed;
-            }
 
             if (Input.GetKey(KeyCode.Space) && isGround)
             {
                 _gravityVelocity.y = Mathf.Sqrt(jumpPower * -2f * gravity);
             }
 
-            _animaParamSpeed = Mathf.Clamp(_animaParamSpeed, 0.0f, 0.5f);
-            _animator.SetFloat("Speed", _animaParamSpeed);
+            // Android Control
+            if (_moveJoystick != null)
+            {
+                if (Mathf.Abs(_moveJoystick.Vertical) > 0.15f)
+                    move += facing * _moveJoystick.Vertical;
+                if (Mathf.Abs(_moveJoystick.Horizontal) > 0.15f)
+                    move += (new Vector3(facing.z, 0, -facing.x) * _moveJoystick.Horizontal);
+            }
         }
+        else
+        {
+            // android
+            
+        }
+
+        if (move != Vector3.zero)
+        {
+            Quaternion facingRotation = Quaternion.LookRotation(move);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                facingRotation,
+                facingSpeed * Time.deltaTime);
+            _animaParamSpeed += Time.deltaTime * speed;
+            _characterController.Move(move * speed * Time.deltaTime);
+        }
+        else
+        {
+            _animaParamSpeed -= Time.deltaTime * speed;
+        }
+        _animaParamSpeed = Mathf.Clamp(_animaParamSpeed, 0.0f, 0.5f);
+        _animator.SetFloat("Speed", _animaParamSpeed);
         
     }
 
