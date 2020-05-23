4cb60894cd83a73b75feb4c823f752c27f565314
mosquitoYee <kenwen1999@gmail.com>
2020-05-24 00:56:06 +0800

    [Update]Map

---
+++ Assets/Scripts/GateAOpen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateAOpen : MonoBehaviour
+{
+	public Animator gateL, gateR;
+    // Start is called before the first frame update
+    void Start()
+    {
+		gateL.enabled = false;
+		gateR.enabled = false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.tag=="Player")
+		{
+			if(Input.GetKey(KeyCode.E))
+			{
+				gateL.enabled = true;
+				gateR.enabled = true;
+			}
+		}
+	}
+}
