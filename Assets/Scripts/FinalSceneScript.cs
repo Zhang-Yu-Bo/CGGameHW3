6ed14d127d351990dd52b78950d4f91d81181c5e
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-05-24 16:23:42 +0800

    [Update] Final Scene Complete

--- Assets/Scripts/FinalSceneScript.cs
+++ Assets/Scripts/FinalSceneScript.cs
@@ -1,17 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FinalSceneScript : MonoBehaviour
 {
+    public GameObject mainCharacter;
+    public GameObject alice;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey("CharacterRecord"))
+        {
+            if (PlayerPrefs.GetInt("CharacterRecord") == 0)
+            {
+                alice.SetActive(true);
+                mainCharacter.SetActive(false);
+            }
+            else
+            {
+                alice.SetActive(false);
+                mainCharacter.SetActive(true);
+            }
+        }
+        else
+        {
+            alice.SetActive(false);
+            mainCharacter.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BackToMenu()
     {
-        
+        SceneManager.LoadScene(0);
     }
 }
